19faa99248d646753f6e6818b19ecd02e740e0ad
user <brewbaker@yandex.com>
2017-08-04 03:31:12 +0300

    Update p.php

--- p.php
+++ p.php
@@ -6,7 +6,13 @@
 		'payment_method'	=> 'qiwi'
 		);
 		$res = $Lbc_Public->BuyBitcoinsOnline($pagination,$arguments);
-		echo '<div class="container"><pre style="height: 65vh;">';
-		print_r($res);
-		echo '</pre></div>';
+        $res = json_decode($json,true);
+        foreach($object->data->ad_list as $current)
+        {
+echo $res;
+echo ($current->trade_type);
+echo ('</br></br>');
+}
+
+
 		?>
